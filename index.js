ff2abec9f751b605742184e68f16de7888df2372
Carlos Nah <cnah27@gmail.com>
2020-01-02 18:21:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,11 +57,14 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  if(cart[item]) {
-    cart.splice(cart.indexOf(item), 1);
+  for(let i = 0; i < cart.length; i++) {
+    if(cart[i].itemName == item) {
+    cart.splice(i, 1);
     console.log("me: ", cart.indexOf(item))
     return cart;
   }
+  }
+  
   return 'That item is not in your cart.'; 
   
 }
