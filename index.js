ca08bbf2e36163081954baf1c92ec33cc5c1862d
Carlos Nah <cnah27@gmail.com>
2020-01-02 17:53:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -69,7 +69,8 @@ function placeOrder(cardNumber) {
   // write your code here
   if(cardNumber === undefined) {
     console.log("Sorry, we don't have a credit card on file for you.");
-  } else if(cardNumber) {
+  }
+  if(cardNumber) {
     let msg = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}`;
     cart = [];
   return msg;
