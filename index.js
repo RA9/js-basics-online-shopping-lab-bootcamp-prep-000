96240629b545e9137f0525598755c7a537a7b85b
Carlos Nah <cnah27@gmail.com>
2020-01-02 17:51:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -70,8 +70,9 @@ function placeOrder(cardNumber) {
   if(!cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.");
   } else if(cardNumber) {
+    let msg = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}`;
     cart = [];
-  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}`;
+  return msg;
   }
   
 }
