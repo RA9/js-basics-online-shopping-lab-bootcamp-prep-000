241f8fe735045dfa781b9340a94c75daff93965c
Carlos Nah <cnah27@gmail.com>
2020-01-02 17:32:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,18 +11,55 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ const price = Math.floor(Math.random() * 100)
+ cart.push({itemName: item, itemPrice: price})
+
+ return `${item} has been added to your cart.`
+
+return cart
 }
 
 function viewCart() {
   // write your code here
+  if(!cart.length) {
+    return "Your shopping cart is empty."
+  }
+
+  const itemAndPrices = []
+
+
+  let intro = "In your cart, you have"
+
+ for (let i in cart ) {
+    itemAndPrices.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
+  }
+
+  if (cart.length == 1)
+  {
+        return `${intro} ${itemAndPrices}.`
+   } else if (cart.length > 1)
+   {
+       let lastItem = itemAndPrices.splice(cart.length -1, 1)
+       let end = itemAndPrices.join(', ')
+       return `${intro} ${end}, and ${lastItem}.`
+   }
 }
+
 
 function total() {
   // write your code here
+  let result = 0
+  for(let i in cart) {
+    result += cart[i].itemPrice
+  }
+  return result
 }
 
 function removeFromCart(item) {
   // write your code here
+  if(item) {
+    item.splice()
+  }
 }
 
 function placeOrder(cardNumber) {
